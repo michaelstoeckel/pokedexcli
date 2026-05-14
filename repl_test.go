29780e47a68619800b530a428eba5f47ae6a4f91
Michael Stöckel <konbert@gmail.com>
2026-05-14 19:44:01 +0200

    CH1.L3 Tests

---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "   BATMAN  ",
+			expected: []string{"batman"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if actual == nil {
+			t.Errorf("Empty")
+		}
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("%s and %s do not match", word, expectedWord)
+			}
+		}
+	}
+}
